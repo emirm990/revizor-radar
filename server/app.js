fdcfb3cff7eb2d60978ad72fe94bb7c58a43f6a7
Emir Mekić <emir.mekic@live.com>
2019-12-05 07:47:36 +0100

    removed unused route and added news route

--- server/app.js
+++ server/app.js
@@ -10,8 +10,10 @@ const port = process.env.PORT || 3000;
 
 const stopsilidza = require("./routes/api/stopsilidza");
 const stopsbascarsija = require("./routes/api/stopsbascarsija");
+const news = require("./routes/api/news");
 app.use("/api/stopsilidza", stopsilidza);
 app.use("/api/stopsbascarsija", stopsbascarsija);
-app.post("/", (req, res) => res.send());
+app.use("/api/news", news);
+//app.post("/", (req, res) => res.send());
 
 app.listen(port, () => console.log(`Sever is started on port ${port}`));
