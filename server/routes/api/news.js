13546754859a96d11dc564064c4d7dc14a7d9c9c
Emir Mekić <emir.mekic@live.com>
2019-12-05 07:46:54 +0100

    Create news.js

---
+++ server/routes/api/news.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongodb = require("mongodb");
+const router = express.Router();
+
+const uri =
+  "mongodb+srv://admin:admin@cluster0-b8i1n.gcp.mongodb.net/revizor-radar?retryWrites=true&w=majority";
+
+router.get("/", async (req, res) => {
+  const news = await getNews();
+  res.send(
+    await news
+      .find({})
+      .limit(5)
+      .sort({ dateUpdated: 1 })
+      .toArray()
+  );
+  console.log(news);
+});
+router.post("/", async (req, res) => {
+  const news = await updateNews();
+  console.log(req.body);
+  await news.insertOne({
+    dateUpdated: req.body.dateUpdated,
+    id: req.body.id,
+    name: req.body.name,
+    revizori: req.body.revizori
+  });
+  res.status(201).send("News Updated");
+});
+
+async function getNews() {
+  const client = await mongodb.MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+  return client.db("revizor-radar").collection("news");
+}
+async function updateNews() {
+  const client = await mongodb.MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+  return client.db("revizor-radar").collection("news");
+}
+module.exports = router;
