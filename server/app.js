d207eabd932de3453a9d63fbb4412ed8ca359941
Emir Mekić <emir.mekic@live.com>
2019-12-09 17:25:07 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,15 +5,18 @@ const cors = require("cors");
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 const port = process.env.PORT || 3000;
 
 const stopsilidza = require("./routes/api/stopsilidza");
 const stopsbascarsija = require("./routes/api/stopsbascarsija");
 const news = require("./routes/api/news");
+const auth = require("./routes/api/auth");
 app.use("/api/stopsilidza", stopsilidza);
 app.use("/api/stopsbascarsija", stopsbascarsija);
 app.use("/api/news", news);
+app.use("/api/auth", auth);
 //app.post("/", (req, res) => res.send());
 
 app.listen(port, () => console.log(`Sever is started on port ${port}`));
