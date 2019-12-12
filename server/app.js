63eda10feac7e8b4991bf7299c492abf6cd0e909
Emir Mekić <emir.mekic@live.com>
2019-12-12 15:59:48 +0100

    session and cookies added

--- server/app.js
+++ server/app.js
@@ -1,22 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const app = express();
-
+global.SESS_NAME = 'sid';
+let isAuth = false;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(cookieParser());
+app.use(session({
+    name: SESS_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: "supersecretcookie",
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        sameSite: true,
+        secure: false
+    }
+}));
+app.use((req, res, next) => {
+    const user = req.session.user;
+    if (user) {
+        isAuth = true;
+    }
+    next();
+});
 const port = process.env.PORT || 3000;
 
 const stopsilidza = require("./routes/api/stopsilidza");
 const stopsbascarsija = require("./routes/api/stopsbascarsija");
 const news = require("./routes/api/news");
 const auth = require("./routes/api/auth");
+const logout = require("./routes/api/logout");
 app.use("/api/stopsilidza", stopsilidza);
 app.use("/api/stopsbascarsija", stopsbascarsija);
 app.use("/api/news", news);
 app.use("/api/auth", auth);
-//app.post("/", (req, res) => res.send());
-
+app.use("/api/logout", logout);
 app.listen(port, () => console.log(`Sever is started on port ${port}`));
