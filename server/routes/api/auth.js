d885382946c423473cc7de2cc8e1fc43c7b00981
Emir Mekić <emir.mekic@live.com>
2019-12-12 16:00:19 +0100

    check for valid session before logging in

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -7,29 +7,34 @@ const uri =
 
 router.get("/", async (req, res) => {
     const users = await getUsers();
-    console.log(req.query);
     await users
         .findOne({ email: req.query.email }, function (err, user) {
-            try {
-                bcrypt.compare(req.query.password, user.password, function (err, response) {
-                    if (response) {
-                        res.status(200).send({ response: response, name: user.name })
-                    } else {
-                        res.status(200).send({ error: "Wrong password" })
-                    }
-                })
-            } catch (err) {
-                res.status(200).send({ error: "Wrong email" });
+            if (req.session.user) {
+                res.status(200).send({ response: true, name: req.session.name });
+            } else {
+                try {
+
+                    req.session.user = user.email;
+                    req.session.name = user.name;
+                    bcrypt.compare(req.query.password, user.password, function (err, response) {
+                        if (response) {
+                            res.status(200).send({ response: response, name: user.name })
+                        } else {
+                            res.status(200).send({ error: "Wrong password" })
+                        }
+                    })
+
+                }
+
+                catch (err) {
+                    res.status(200).send({ error: "Wrong email" });
+                }
             }
 
-
-
         })
-
 });
 router.post("/", async (req, res) => {
     const users = await getUsers();
-    console.log();
     bcrypt.genSalt(10, function (err, salt) {
         bcrypt.hash(req.body.password, salt, function (err, hash) {
             users.findOne({ email: req.body.email }, function (err, user) {
